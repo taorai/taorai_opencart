200f530ad335975b0777be43fc1d83db902c446f
Unknown <taorai.asia@gmail.com>
2017-04-01 17:28:34 +0700

    init project

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/taorai/admin/');
+define('HTTP_CATALOG', 'http://localhost/taorai/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/taorai/admin/');
+define('HTTPS_CATALOG', 'http://localhost/taorai/');
+
+// DIR
+define('DIR_APPLICATION', '/home/steven/GitRepository/taorai/admin/');
+define('DIR_SYSTEM', '/home/steven/GitRepository/taorai/system/');
+define('DIR_IMAGE', '/home/steven/GitRepository/taorai/image/');
+define('DIR_LANGUAGE', '/home/steven/GitRepository/taorai/admin/language/');
+define('DIR_TEMPLATE', '/home/steven/GitRepository/taorai/admin/view/template/');
+define('DIR_CONFIG', '/home/steven/GitRepository/taorai/system/config/');
+define('DIR_CACHE', '/home/steven/GitRepository/taorai/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/steven/GitRepository/taorai/system/storage/download/');
+define('DIR_LOGS', '/home/steven/GitRepository/taorai/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/steven/GitRepository/taorai/system/storage/modification/');
+define('DIR_UPLOAD', '/home/steven/GitRepository/taorai/system/storage/upload/');
+define('DIR_CATALOG', '/home/steven/GitRepository/taorai/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'taorai_admin');
+define('DB_PASSWORD', 'bXCr8meDIU8KSxUb');
+define('DB_DATABASE', 'db_taorai_dev');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'taorai_');
